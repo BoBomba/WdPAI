d3ac70568798495ca794669fd701c1c5968e2671
BoBomba <122816717+BoBomba@users.noreply.github.com>
2023-11-15 15:40:13 +0100

    commit lab 7/8 adding MVC and simmilar data

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
 <?php
 
-echo 'Hi there 👋';
+require 'Routing.php';
+
+$path = trim($_SERVER['REQUEST_URI'], '/');
+$path = parse_url( $path, PHP_URL_PATH);
+
+Routing::get('', 'DefaultController');
+Routing::get('students', 'DefaultController');
+
+//echo(__DIR__.$path);
+
+Routing::run($path);

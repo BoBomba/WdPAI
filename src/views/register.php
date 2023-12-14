0907279abe48641a13e4c6102e1d0d45390c45ce
BoBomba <122816717+BoBomba@users.noreply.github.com>
2023-12-14 15:48:26 +0100

    commit nr.9 adding repository and connect to it

---
+++ src/views/register.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./public/css/global.css">
+    <title>Document</title>
+</head>
+
+<body class="login">
+
+    <a href="/dashboard">
+        Powrót
+    </a>
+    <div class="panel_loginu">
+        <div class="login">
+            <!-- TODO  -->
+            <h1>Register</h1>
+            <form action="/register" method="POST">
+                <input type="text" id="firstname">
+                <input type="text" id="lastname">
+                <input type="email">
+                <input type="password">
+                <button type="submit">Sign up</button>
+                <button type="reset">reset</button>
+            </form>
+        </div>
+    </div>
+</body>
+
+</html>
